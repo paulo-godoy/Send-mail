e89147df6bc5b828114ba1fea9fc094093e939f2
Paulo Ricardo Pinto Godoy <pauloricardo.godoy@yahoo.com.br>
2019-05-18 23:45:26 -0300

    upload dos arquivos
    versão final

---
+++ htdocs/app_send_mail/processa_envio.php
@@ -0,0 +1,10 @@
+<?php
+
+require "../../app_send_mail/biblioteca/PHPMailer/Exception.php";
+require "../../app_send_mail/biblioteca/PHPMailer/OAuth.php";
+require "../../app_send_mail/biblioteca/PHPMailer/PHPMailer.php";
+require "../../app_send_mail/biblioteca/PHPMailer/POP3.php";
+require "../../app_send_mail/biblioteca/PHPMailer/SMTP.php";
+
+require "../../app_send_mail/processa_envio.php";
+    
